4a6ba24fb5212775cbc5d6fed0eec4ad4775b93c
vanshsiwach2625-glitch <vanshsiwach2625@gmail.com>
2026-09-14 11:23:06 +0530

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a number\n");
+    scanf("%d" ,&a);
+    if(a%2==0)
+    {
+        printf("%d is an even number" ,a);
+    }
+    else
+    {
+        printf("%d is an odd number",a);
+    }
+
+}
